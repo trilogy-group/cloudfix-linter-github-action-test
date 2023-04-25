404dbfa9d49e62aa1f583e4c07b79a656c396322
Yogesh Kumar <yogesh.kumar@trilogy.com>
2023-04-25 18:09:31 +0000

    Push new tag to deployed resources

--- main.tf
+++ main.tf
@@ -27,8 +27,8 @@ provider "aws" {
 resource "aws_s3_bucket" "prasheel-test-bucket" {
   bucket_prefix = "my-tf-bucket-cloudfixlinter"
   tags = {
-    Owner     = "prasheel.tiwari@trilogy.com"
-    yor_trace = "1bf819f5-d130-4f67-810e-d6e24e32e125"
+    Owner                       = "prasheel.tiwari@trilogy.com"
+    "cloudfix:linter_yor_trace" = "97947a7b-2986-41b7-a6fb-a64ca78a4d07"
   }
 }
 
@@ -37,7 +37,7 @@ resource "aws_ebs_volume" "data-vol" {
   size              = 1
   type              = "gp2"
   tags = {
-    Owner     = "prasheel.tiwari@trilogy.com"
-    yor_trace = "2e9ace76-9d14-4a56-8cf4-0a90393b5de6"
+    Owner                       = "prasheel.tiwari@trilogy.com"
+    "cloudfix:linter_yor_trace" = "23075b98-f180-4c01-a86d-d91c8ef9e6e7"
   }
 }
