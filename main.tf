4027ec666ca044d461cc840d21c618e20266a45e
Yogesh Kumar <yogesh.kumar@trilogy.com>
2023-04-26 10:41:37 +0000

    Fresh deploy

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "prasheel-test-organization"
+    organization = "yogesh-ti-org"
 
     workspaces {
       name = "gh-actions-demo"
@@ -24,20 +24,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "prasheel-test-bucket" {
+resource "aws_s3_bucket" "cloudfix-linter-gh-action-bucket" {
   bucket_prefix = "my-tf-bucket-cloudfixlinter"
   tags = {
-    Owner                       = "prasheel.tiwari@trilogy.com"
+    Owner                       = "yogesh.kumar@trilogy.com"
     "cloudfix:linter_yor_trace" = "97947a7b-2986-41b7-a6fb-a64ca78a4d07"
   }
 }
 
-resource "aws_ebs_volume" "data-vol" {
+resource "aws_ebs_volume" "cloudfix-linter-gh-action-data-vol" {
   availability_zone = "us-east-1a"
   size              = 1
   type              = "gp2"
   tags = {
-    Owner                       = "prasheel.tiwari@trilogy.com"
+    Owner                       = "yogesh.kumar@trilogy.com"
     "cloudfix:linter_yor_trace" = "23075b98-f180-4c01-a86d-d91c8ef9e6e7"
   }
 }
